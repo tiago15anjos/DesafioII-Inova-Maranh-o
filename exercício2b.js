667777ab8a980b4febeb5de610c17855aea10d1a
Tiago Anjos <164925389+tiago15anjos@users.noreply.github.com>
2024-05-02 19:08:40 -0300

    Update exercício2b.js
    Correção da mensagem de erro. 
            capitalAplicado = parseFloat(prompt("Valor informado inválido! Por favor, informe novamente o valor depositado na poupança:"));

--- exercício2b.js
+++ exercício2b.js
@@ -1,11 +1,11 @@
-function calcularRendimentoIntermediario() {
+function rendimento() {
     let capitalAplicado = parseFloat(prompt("Por favor, informe o valor depositado na poupança:"));
     let tempoDeAplicacao = parseInt(prompt("Informe a quantidade de meses da aplicação:"));
     let taxaDeJuros; 
 
     while (isNaN(capitalAplicado) || capitalAplicado < 0 || isNaN(tempoDeAplicacao) || tempoDeAplicacao < 0) {
         alert("Valor informado inválido! Por favor, informe o valor depositado na poupança e o tempo em meses corretamente.");
-        capitalAplicado = parseFloat(prompt("Por favor, informe novamente o valor depositado na poupança:"));
+        capitalAplicado = parseFloat(prompt("Valor informado inválido! Por favor, informe novamente o valor depositado na poupança:"));
         tempoDeAplicacao = parseInt(prompt("Informe novamente a quantidade de meses da aplicação:"));
     }
 
@@ -21,4 +21,4 @@ function calcularRendimentoIntermediario() {
     alert(`A quantia de R$${capitalAplicado.toFixed(2)} aplicada em ${tempoDeAplicacao} meses, rendeu R$${valorRendimento.toFixed(2)} a juros de ${(taxaDeJuros * 100).toFixed(4)}%.`);
 }
 
-calcularRendimentoIntermediario();
+rendimento();
