3dddc76b742d8edc45f724e0ad2e77bac3b0f886
Tiago Anjos <164925389+tiago15anjos@users.noreply.github.com>
2024-05-02 15:03:24 -0300

    Exercício 2b
    b) Faça um algoritmo que, se o capital aplicado for maior ou igual a R$999,99 e menor ou igual R$10.000,00 e o tempo de aplicação for maior que 5 meses e menor que 12 meses, a taxa de rendimento deve ser 2%. O algoritmo deve solicitar ao cliente o valor do capital aplicado e o tempo de aplicação e, após a operação, o programa deve exibir na tela, o valor aplicado, a quantidade de meses em aplicação e o valor de rendimento em R$, utilizando o “Tamplate string”, da seguinte forma: **A quantia de R$[CapitalAplicado] aplicada em [TempoDeAplicacao] meses, rendeu R$[ValorRendimento].** Caso o valor informado for menor que 0 ou diferente de um número, o algoritmo deve apresentar uma alerta na tela com a seguinte mensagem: “Valor informado inválido! Por favor, informe o valor depositado na poupança”. Em seguida, solicite novamente ao cliente o valor aplicado na poupança e a quantidade de meses, até que o valor válido seja informado.

---
+++ exercício2b.js
@@ -0,0 +1,24 @@
+function calcularRendimentoIntermediario() {
+    let capitalAplicado = parseFloat(prompt("Por favor, informe o valor depositado na poupança:"));
+    let tempoDeAplicacao = parseInt(prompt("Informe a quantidade de meses da aplicação:"));
+    let taxaDeJuros; 
+
+    while (isNaN(capitalAplicado) || capitalAplicado < 0 || isNaN(tempoDeAplicacao) || tempoDeAplicacao < 0) {
+        alert("Valor informado inválido! Por favor, informe o valor depositado na poupança e o tempo em meses corretamente.");
+        capitalAplicado = parseFloat(prompt("Por favor, informe novamente o valor depositado na poupança:"));
+        tempoDeAplicacao = parseInt(prompt("Informe novamente a quantidade de meses da aplicação:"));
+    }
+
+    if (capitalAplicado >= 999.99 && capitalAplicado <= 10000) {
+        taxaDeJuros = 2 / 100;  // Taxa de juros de 2%
+    } else {
+        taxaDeJuros = 0.6022 / 100;  // Taxa de juros padrão de 0,6022% ao mês
+    }
+
+    let valorFinal = capitalAplicado * Math.pow(1 + taxaDeJuros, tempoDeAplicacao);
+    let valorRendimento = valorFinal - capitalAplicado;
+
+    alert(`A quantia de R$${capitalAplicado.toFixed(2)} aplicada em ${tempoDeAplicacao} meses, rendeu R$${valorRendimento.toFixed(2)} a juros de ${(taxaDeJuros * 100).toFixed(4)}%.`);
+}
+
+calcularRendimentoIntermediario();
